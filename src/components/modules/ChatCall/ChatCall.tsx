283ab5f220ad24c0dcd15978b4ba87319e546b3c
huy21it <huy678d@gmail.com>
2022-11-01 20:36:45 +0700

    fix: classname

--- src/components/modules/ChatCall/ChatCall.tsx
+++ src/components/modules/ChatCall/ChatCall.tsx
@@ -15,8 +15,8 @@ interface IChatCallProps {
 const ChatCall: React.FC<IChatCallProps> = ({ onClose, title }) => {
   return (
     <div className="chat-call">
-      <div className="modal__overlay"></div>
-      <div className="modal__body">
+      <div className="modal-overlay"></div>
+      <div className="modal-body">
         <div className="modal-body__items">
           <Avatar
             path="https://scontent.fdad3-1.fna.fbcdn.net/v/t39.30808-6/277551484_1607305416300980_1426726336589949572_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=NDE6kmkwFQ8AX9-U3bh&_nc_ht=scontent.fdad3-1.fna&oh=00_AT8SGcvhT_y6-Lc16cMBv0OwsUOg0x7ef7Yp1yb_1teoEQ&oe=635BDBD2"
